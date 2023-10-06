9b3782772f39ed35f9ced108122a8b4d911ca218
unknown <INT202&204@5CD3019B4F.control.ad.sit.kmutt.ac.th>
2023-10-06 12:52:53 +0700

    Add cookie theme

--- registerfri/src/main/java/sit/int202/registerfri/servlets/CourseListServlet.java
+++ registerfri/src/main/java/sit/int202/registerfri/servlets/CourseListServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import sit.int202.registerfri.models.CourseRepository;
 import sit.int202.registerfri.models.Semester;
+import sit.int202.registerfri.models.ViewSupport;
+
 
 import java.io.IOException;
 import java.util.Map;
@@ -13,22 +15,28 @@ import java.util.Map;
 public class CourseListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("semesters", Semester.getAllSemesterText());
-        getServletContext().getRequestDispatcher("/course_list.jsp").forward(request,response);
+        ServletContext sc = getServletContext();
+        if (sc.getAttribute("semesters") == null) {
+            sc.setAttribute("semesters", Semester.getAllSemesterText());
+        }
+        getServletContext().getRequestDispatcher("/course_list.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Map<String, String[]> parameterMap = request.getParameterMap();
         request.setCharacterEncoding("UTF-8");
-        if (parameterMap.get("semester")==null) {
-            doGet(request,response);
+        if (parameterMap.get("semester") == null) {
+            doGet(request, response);
             return;
         }
         int semester = Integer.valueOf(parameterMap.get("semester")[0]);
-        request.setAttribute("semesters", Semester.getAllSemesterText());
+//        request.setAttribute("semesters", Semester.getAllSemesterText());
         request.setAttribute("selectedSemester", semester);
         request.setAttribute("subjects", CourseRepository.getSubjects(semester));
-        getServletContext().getRequestDispatcher("/course_list.jsp").forward(request,response);
+        //---- add new ----
+        request.setAttribute("existSubjects", ViewSupport.getExistSubject(semester, request.getSession(false)));
+        //----
+        getServletContext().getRequestDispatcher("/course_list.jsp").forward(request, response);
     }
 }
--- registerfri/target/registerfri-1.0-SNAPSHOT/course_list.jsp
+++ registerfri/target/registerfri-1.0-SNAPSHOT/course_list.jsp
@@ -6,6 +6,7 @@
 <%--    <link rel="stylesheet" type="text/css" href="assets/bootstrap-4.5/css/bootstrap.min.css"/>--%>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
 </head>
 <body>
 <div class="container m-auto bg-light w-75">
@@ -43,7 +44,8 @@
                         <div class="col-6">${subject.title}</div>
                         <div class="col-1">${subject.credit}</div>
                         <div class="col-1">
-                            <input type="checkbox" name="registeredSubjects" value="${subject.subjectId}">
+                            <input ${existSubjects[subject.subjectId] ? 'checked' : ''}
+                                    type="checkbox" name="registeredSubjects" value="${subject.subjectId}">
                         </div>
                     </div>
                 </c:forEach>
